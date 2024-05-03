4428012b8c35e122e90f1a7113d597d5ccbe1e53
Your Name <your.email@example.com>
2024-05-03 10:21:20 -0700

    Adjusted styles of app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-<p class="text-blue-800 flex justify-center items-center *
-text-2xl"> Hi</p>
-<h1> Hello</h1>
+  <div class="mt-24 container mx-auto flex-col flex justify-center items-center">
+<p class="text-blue-800  
+text-2xl"> Hi.</p>
+<h1> Hello!</h1>
   </div>
 </template>
